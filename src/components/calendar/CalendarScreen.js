a9f8d99eda7aa2b4e8cf44d3b2cc1e9bf751a9a5
Santiago Pegels <santiagopegels@gmail.com>
2021-07-07 10:48:13 -0300

    spanish Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import 'moment/locale/es'
+
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Navbar } from '../ui/Navbar'
+import { messages } from '../../helpers/calendar-message-es'
+
+moment.locale('es')
 
 const localizer = momentLocalizer(moment)
 
 const events = [{
     title: 'Cumpleaños',
     start: moment().toDate(),
-    end: moment().add(2, 'hours').toDate()
+    end: moment().add(2, 'hours').toDate(),
+    notes: 'Comprar comida'
 }]
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white',
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -22,7 +44,8 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
-                style={{ height: 500 }}
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
             />
         </div>
     )
